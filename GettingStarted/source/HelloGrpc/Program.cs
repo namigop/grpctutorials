b4c48879186b2037ee63349f645ca5139ad30065
erik.araojo@wcfstorm.com <erik.araojo@wcfstorm.com>
2024-01-18 12:27:02 +0800

    Getting started project

---
+++ GettingStarted/source/HelloGrpc/Program.cs
@@ -0,0 +1,16 @@
+using HelloGrpc.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGrpcReflectionService();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
